e77198a607d4adbfedb9693352ac029b5f22e204
rafael.voltz <rafael.voltz@JVN60102038.jv01.local>
2019-10-28 15:52:39 -0300

    Ajustado builder e criado atributos de limite e saldo.

--- src/main/java/com/totvs/tj/tcc/domain/conta/Conta.java
+++ src/main/java/com/totvs/tj/tcc/domain/conta/Conta.java
@@ -2,35 +2,115 @@ package com.totvs.tj.tcc.domain.conta;
 
 import static com.totvs.tj.tcc.domain.conta.Conta.Situacao.ABERTO;
 import static com.totvs.tj.tcc.domain.conta.Conta.Situacao.SUSPENSO;
-import static lombok.AccessLevel.PRIVATE;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import java.util.Objects;
+
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
-@Builder
-@AllArgsConstructor(access = PRIVATE)
 public class Conta {
 
     private ContaId id;
     
     private Empresa empresa;
     
-    private ResponsavelId responsavel;
+    private Gerente gerente;
 
     private Situacao situacao;
     
+    private final double maximoLimite = 15000;
+    
+    private double saldo;
+    
+    private final double limite;
+    
+    private Conta(Builder builder) {
+        this.id = builder.id;
+        this.empresa  = Objects.requireNonNull(builder.empresa);
+        this.gerente  = Objects.requireNonNull(builder.gerente);
+        this.situacao = Objects.requireNonNull(builder.situacao);
+        this.saldo    = builder.saldo;
+        this.limite   = builder.limite;
+    }
+    
+
+    public static Builder builder() {
+        return new Builder();
+    }
+        
     public void suspender() {
         situacao = SUSPENSO;
     }
 
     public boolean isDisponivel() {
         return ABERTO.equals(situacao);
-    }    
+    }
+    
+    
+    
    
+    public static class Builder {
+
+        private ContaId id;
+        
+        private Empresa empresa;
+        
+        private Gerente gerente;
+
+        private Situacao situacao;
+        private final double maximoLimite = 15000;
+        
+        private double saldo;
+        private double limite;
+
+        public Builder id(ContaId id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder empresa(Empresa empresa) {
+            this.empresa = empresa;
+            return this;
+        }
+
+        public Builder gerente(Gerente gerente) {
+            this.gerente = gerente;
+            return this;
+        }
+
+        public Builder situacao(Situacao situacao) {
+            this.situacao = situacao;
+            return this;
+        }
+        
+        public Conta build() {
+            return new Conta(this);
+        }
+
+        public Conta buildAsNew() {
+            this.saldo = 0;
+            this.situacao = Situacao.ABERTO;
+            this.limite = getLimiteInicial();
+            return this.build();
+        }
+        
+        public double getLimiteInicial() {        
+            if (isLimiteMaximo())
+               return this.maximoLimite;
+            
+            return empresa.getNumerosDeFuncionarios() * empresa.getValorMercadoEmpresa();
+        }
+        
+        public boolean isLimiteMaximo() {
+           if (empresa.getNumerosDeFuncionarios() * empresa.getValorMercadoEmpresa() > this.maximoLimite)
+               return true;
+           return false;
+        }
+        
+    }
+    
     static enum Situacao {
 
         ABERTO, SUSPENSO;
--- src/test/java/com/totvs/tj/tcc/ContaTest.java
+++ src/test/java/com/totvs/tj/tcc/ContaTest.java
@@ -8,13 +8,13 @@ import java.util.Map;
 
 import org.junit.Test;
 
-import com.totvs.tj.tcc.app.AbrirContaCommand;
-import com.totvs.tj.tcc.app.ContaApplicationService;
 import com.totvs.tj.tcc.domain.conta.Conta;
 import com.totvs.tj.tcc.domain.conta.ContaId;
 import com.totvs.tj.tcc.domain.conta.ContaRepository;
 import com.totvs.tj.tcc.domain.conta.Empresa;
 import com.totvs.tj.tcc.domain.conta.EmpresaId;
+import com.totvs.tj.tcc.domain.conta.Gerente;
+import com.totvs.tj.tcc.domain.conta.GerenteId;
 import com.totvs.tj.tcc.domain.conta.Responsavel;
 import com.totvs.tj.tcc.domain.conta.ResponsavelId;
 
@@ -26,9 +26,15 @@ public class ContaTest {
 
     private final ResponsavelId idResponsavel = ResponsavelId.generate();
     
-    private final double valorMercadoEmpresa = 10000.00;
+    private final GerenteId idGerente = GerenteId.generate();
     
-    private final int numerosDeFuncionarios = 12;
+    private final double valorMercadoEmpresaNormal = 1000.00;
+    
+    private final int numerosDeFuncionariosNormal = 12;
+    
+    private final double valorMercadoEmpresaLimite = 10000.00;
+    
+    private final int numerosDeFuncionariosLimite = 12;
     
 
     @Test
@@ -39,31 +45,75 @@ public class ContaTest {
                 .id(idResponsavel)           
             .build();
         
+        Gerente gerente = Gerente.builder()
+                .id(idGerente)
+                .build();
+        
         Empresa empresa = Empresa.builder()
                 .id(idEmpresa)
                 .responsavel(responsavel)
-                .numerosDeFuncionarios(numerosDeFuncionarios)
-                .valorMercadoEmpresa(valorMercadoEmpresa)
+                .numerosDeFuncionarios(numerosDeFuncionariosNormal)
+                .valorMercadoEmpresa(valorMercadoEmpresaNormal)
             .build();
         
         Conta conta = Conta.builder()
                 .id(idConta)
                 .empresa(empresa)
-                .responsavel(idResponsavel)
-            .build();
+                .gerente(gerente)
+            .buildAsNew();
 
         // THEN
         assertNotNull(conta);
-
+        
         assertEquals(idConta, conta.getId());
         assertEquals(empresa, conta.getEmpresa());
-        assertEquals(idResponsavel, conta.getResponsavel());
+        assertEquals(gerente, conta.getGerente());
+        assertEquals(numerosDeFuncionariosNormal * valorMercadoEmpresaNormal, conta.getLimite(), 0);
+        
+        assertEquals(idConta.toString(), conta.getId().toString());
+        assertEquals(empresa.toString(), conta.getEmpresa().toString());
+        assertEquals(gerente.toString(), conta.getGerente().toString());
+    }
+    
+    @Test
+    public void aoCriarUmaContaLimiteMaximo() throws Exception {
+
+        // WHEN
+        Responsavel responsavel = Responsavel.builder()
+                .id(idResponsavel)           
+            .build();
+        
+        Gerente gerente = Gerente.builder()
+                .id(idGerente)
+                .build();
+        
+        Empresa empresa = Empresa.builder()
+                .id(idEmpresa)
+                .responsavel(responsavel)
+                .numerosDeFuncionarios(numerosDeFuncionariosLimite)
+                .valorMercadoEmpresa(valorMercadoEmpresaLimite)
+            .build();
+        
+        Conta conta = Conta.builder()
+                .id(idConta)
+                .empresa(empresa)
+                .gerente(gerente)
+            .buildAsNew();
+
+        // THEN
+        assertNotNull(conta);
+        
+        assertEquals(idConta, conta.getId());
+        assertEquals(empresa, conta.getEmpresa());
+        assertEquals(gerente, conta.getGerente());
+        assertEquals(conta.getMaximoLimite(), conta.getLimite(), 0);
 
         assertEquals(idConta.toString(), conta.getId().toString());
         assertEquals(empresa.toString(), conta.getEmpresa().toString());
-        assertEquals(idResponsavel.toString(), conta.getResponsavel().toString());
+        assertEquals(gerente.toString(), conta.getGerente().toString());
     }
 
+    /*
    @Test
     public void aoSolicitarAberturaConta() throws Exception {
 
